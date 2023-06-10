02d55822c2e6f025806388c4a924f0aa79e6e117
black112301 <01eramirez01@gmail.com>
2023-06-09 23:50:28 -0700

    added some images

--- src/components/AlienListItem.tsx
+++ src/components/AlienListItem.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import { Alien } from '../utils/Interfaces';
-import testImg from '../assets/images/Heatblast.png';
-import testImg2 from '../assets/images/HeatblastButton.png';
+import testImg from '../assets/images/Terraspin.png';
+import testImg2 from '../assets/images/TerraspinButton.png';
 import testImg3 from '/omnitrix.svg';
 
 interface AlienListItemProps {
@@ -22,12 +22,12 @@ export default function AlienListItem({ filteredAlienList }: AlienListItemProps)
           <div key={alien.id}>
             <button
               onClick={() => handleClick(alien)}
-              className='font-semibold bg-green-500 text-white px-4 py-2 rounded-lg h-20 flex items-center w-full hover:scale-105 relative'
+              className='font-semibold bg-gradient-to-r from-emerald-500 to-[#22c55e31] px-4 py-2 rounded-lg h-20 flex items-center w-full hover:scale-105 relative border-2 border-black'
             >
-              <img src={testImg3} alt={alien.name} className='absolute inset-0 h-full w-20 ml-1.5 mt-0.5 object-contatin mix-blend-lighten opacity-40' />
-              <img src={testImg2} alt={alien.name} className='absolute inset-0 h-full w-full object-cover mix-blend-luminosity opacity-20' />
+              <img src={testImg3} alt={alien.name} className='absolute inset-0 h-full w-20 ml-1 mt-0.5 object-contatin mix-blend-lighten opacity-40' />
+              <img src={testImg2} alt={alien.name} className='absolute inset-0 h-full w-full object-cover mix-blend-luminosity opacity-50' />
               <img src={testImg} alt={alien.name} className='absolute h-5/6 left-0 m-2 z-10' />
-              <span className='font-extrabold text-2xl text-white p-2 mix-blend-luminosity absolute top-0 left-20'>{alien.name.toUpperCase()}</span>
+              <span className='font-extrabold text-xl text-white p-2 absolute left-[72px]'>{alien.name.toUpperCase()}</span>
             </button>
           </div>
         );
