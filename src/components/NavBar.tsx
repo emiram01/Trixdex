65a909eefd1a645924dddd76fe21af0e71e5de97
black112301 <01eramirez01@gmail.com>
2023-06-15 17:11:37 -0700

    fixed image refs for deployment

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,13 +4,13 @@ import infoicon from '../assets/images/icons/infoicon.svg';
 export default function Navbar() {
   return (
     <div className='bg-gray-800 border-b-2 border-black'>
-      <nav className="container mx-auto py-4 px-6 flex justify-between items-center">
-        <NavLink to={'/'} className="flex items-center text-white text-lg font-extrabold hover:brightness-75">
-          <img src="/omnitrix.svg" alt="Logo" className="h-8 mr-2" />
+      <nav className='container mx-auto py-4 px-6 flex justify-between items-center'>
+        <NavLink to={'/'} className='flex items-center text-white text-lg font-extrabold hover:brightness-75'>
+          <img src='./omnitrix.svg' alt='Logo' className='h-8 mr-2' />
           TRIXDEX
         </NavLink>
-        <NavLink to={'info'} className="flex items-center text-white text-lg font-medium hover:brightness-75">
-          <img src={infoicon} alt="Logo" className="h-6 mr-2" />
+        <NavLink to={'info'} className='flex items-center text-white text-lg font-medium hover:brightness-75'>
+          <img src={infoicon} alt='Logo' className='h-6 mr-2' />
           Info
         </NavLink>
       </nav>
