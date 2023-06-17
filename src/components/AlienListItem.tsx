db6de4a27541db12a1b0f0facb4433b6d200b9ca
black112301 <01eramirez01@gmail.com>
2023-06-16 18:25:05 -0700

    fixed routing

--- src/components/AlienListItem.tsx
+++ src/components/AlienListItem.tsx
@@ -21,7 +21,7 @@ export default function AlienListItem({ filteredAlienList }: AlienListItemProps)
               onClick={() => handleClick(alien)}
               className='font-semibold bg-gradient-to-r from-emerald-500 to-[#22c55e31] px-4 py-2 rounded-lg h-20 flex items-center w-full hover:scale-105 relative border-2 border-black'
             >
-              <img src={import.meta.env.BASE_URL + '/omnitrix.svg'} className='absolute inset-0 h-full w-20 ml-1 mt-0.5 object-contatin mix-blend-lighten opacity-40' />
+              <img src={import.meta.env.BASE_URL + 'omnitrix.svg'} className='absolute inset-0 h-full w-20 ml-1 mt-0.5 object-contatin mix-blend-lighten opacity-40' />
               <img src={import.meta.env.BASE_URL + alien.images[1]} className='absolute inset-0 h-full w-full object-cover mix-blend-luminosity opacity-50' />
               <img src={import.meta.env.BASE_URL + alien.images[0]} className='absolute h-5/6 left-0 m-2 z-10' />
               <span className='font-extrabold text-l sm:text-xl text-white p-2 absolute left-20'>{alien.name.toUpperCase()}</span>
